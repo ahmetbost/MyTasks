0e1de1919f7cc6506f769160559c762049e5061d
ahmetbost <ahmetbostanciogluu@gmail.com>
2025-05-10 17:38:42 +0300

    Initial Commit

---
+++ MyTasks/MyTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTasksApp.swift
+//  MyTasks
+//
+//  Created by Ahmet Bostancıoğlu on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
